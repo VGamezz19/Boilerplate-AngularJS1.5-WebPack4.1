35d421e50b585291c7d4883dd5a0b586f7cace16
Victor Gamez <victor.gamez1596@gmail.com>
2018-06-09 16:58:52 +0200

    less prod

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(css|scss)$/,
+        test: /\.(css|less)$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
       }
     ]
